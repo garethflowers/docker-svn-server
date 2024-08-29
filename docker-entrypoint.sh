ce38cc835ea61a9b7a3944788b3916ac75929af0
Guiorgy <Guiorgy123@gmail.com>
2024-08-29 16:27:09 +0400

    use trap to redirect SIGTERM
    Also replaced cat with a while to avoid creating a subshell.
    Together, this allows for proper handling of the SIGTERM signal when stopping the container.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,14 @@ rm -f /var/svn/svnlogs
 mkfifo /var/svn/svnlogs
 
 # run the SVN service in the background
-/usr/bin/svnserve --daemon --root /var/opt/svn --log-file=/var/svn/svnlogs &
+/usr/bin/svnserve --daemon --foreground --root /var/opt/svn --log-file=/var/svn/svnlogs &
 # remember the PID of the SVN server
 SVNSERVE_PID=$!
 
-# redirect svnserve logs to stdout
-cat /var/svn/svnlogs
+# redirect SIGTERM to the SVN service
+trap "kill -s SIGTERM $SVNSERVE_PID" SIGTERM
 
-# stop the SVN server running in the background
-kill -s SIGTERM $SVNSERVE_PID
+# redirect svnserve logs to stdout
+while read -r line < /var/svn/svnlogs; do
+	printf '%s\n' "$line"
+done
